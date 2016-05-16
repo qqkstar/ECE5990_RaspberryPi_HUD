667f203a0b3e5edeaeb2b3d318705dd24e5b8866
qqkstar <xz254@cornell.edu>
2016-05-16 00:42:14 -0400

    something is wrong with temp sensor

--- temp_read.py
+++ temp_read.py
@@ -9,7 +9,7 @@ from kalman_filter import *
 from LSM9DS0 import *
 
 bus = smbus.SMBus(1);         # 0 for R-Pi Rev. 1, 1 for Rev. 2
-std_dev = 1.23
+std_dev = 0.48
 process_variance = 1e-5
 estimated_measurement_variance = std_dev ** 2  # 0.05 ** 2
 kalman_filter = KalmanFilter(process_variance, estimated_measurement_variance)
@@ -21,31 +21,39 @@ def writeTMP(register, value):
 def readTMP():
   tmp_l = bus.read_byte_data(TMP_ADDRESS,  OUT_TEMP_L_XM)
   tmp_h = bus.read_byte_data(TMP_ADDRESS,  OUT_TEMP_H_XM)
-  tmp_combined = (tmp_l | tmp_h << 8)
-  tmp_combined = tmp_combined if tmp_combined < 32768 else tmp_combined - 65536
-  return tmp_combined
-
-def calibTMP():
-  tmp_calibration = []
-  writeTMP(CTRL_REG5_XM, 0b11110000)  #TMP enable, M data rate = 50Hz
-  while len(tmp_calibration) < 500:
-    tmp_calibration.append(readTMP())
-  std_dev = numpy.std(tmp_calibration)
-  print "Standard deviation of sensor is", std_dev
-  estimated_measurement_variance = std_dev ** 2  # 0.05 ** 2
-  kalman_filter = KalmanFilter(process_variance, estimated_measurement_variance)
+  print tmp_l
+  print tmp_h
+  tmp_combined = ((tmp_h & 0b00001111) << 8) | tmp_l
+  tmp_combined = tmp_combined if tmp_combined < 2048 else tmp_combined - 4096
+  return tmp_combined     # add 16 as calibration factor
 
 #initialize temperature sensor
 def initTMP():
   writeTMP(CTRL_REG5_XM, 0b11110000)  #TMP enable, M data rate = 50Hz
 
+#Calibrate temperature sensor using latest sensor data, mainly acquire std_dev value
+def calibTMP():
+  tmp_calibration = []
+  while len(tmp_calibration) < 500:
+    tmp_value = readTMP()
+    tmp_calibration.append(tmp_value)
+  print len(set(tmp_calibration))
+  std_dev = numpy.std(tmp_calibration)
+  print "Standard deviation of sensor is", std_dev
+  estimated_measurement_variance = std_dev ** 2  # 0.05 ** 2
+  kalman_filter = KalmanFilter(process_variance, estimated_measurement_variance)
+  for iteration in xrange(1, len(tmp_calibration)):
+    kalman_filter.input_latest_noisy_measurement(tmp_calibration[iteration])
+
+#Obtain kalman-filtered temperature in Celsius, rounded to integer form
 def getTMP():
   tmp_readings = []
-  while len(tmp_readings) < 200:
+  while len(tmp_readings) < 100:
     tmp_readings.append(readTMP())
   iteration_count = len(tmp_readings)
   for iteration in xrange(1, iteration_count):
     kalman_filter.input_latest_noisy_measurement(tmp_readings[iteration])
   temperature = kalman_filter.get_latest_estimated_measurement()
   print int(round(temperature))
-  return kalman_filter.get_latest_estimated_measurement()
+  return temperature 
+
